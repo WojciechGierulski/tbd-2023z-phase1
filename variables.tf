2e7e69c5ddbb6e5a5caa0d8d4870455997578d9c
jakubpludowski <jakubpludowski@github.com>
2023-12-03 13:02:55 +0000

    qucik fix variables

--- variables.tf
+++ variables.tf
@@ -19,3 +19,18 @@ variable "dataproc_num_preemptible_nodes" {
   description = "The number of preemptible/spot instances for the Dataproc cluster"
   default     = 0
 }
+
+variable "jupyterlab_machine_type" {
+  description = "The machine type for the JupyterLab instance"
+  default     = "e2-standard-2"
+}
+
+variable "dataproc_machine_type" {
+  description = "The machine type for the Dataproc cluster"
+  default     = "e2-standard-2"
+}
+
+variable "dataproc_num_worker_nodes" {
+  description = "The number of worker nodes for the Dataproc cluster"
+  default     = 2
+}
